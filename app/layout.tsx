3597b2e41b2d93adfecdb87d1d57a1a2de3f5cdd
dev-xero <elo48858@gmail.com>
2024-10-11 20:05:59 +0100

    conf: Update opengraph configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,35 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Inter } from 'next/font/google'
+import type { Metadata } from 'next';
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "ChessIO | Play and Challenge Others!",
-  description: "ChessIO is a hobby 1v1 chess challenge web app for friends.",
+    title: 'ChessIO | Play and Challenge Others!',
+    description: 'ChessIO is a hobby 1v1 chess challenge web app for friends.',
+    openGraph: {
+        title: 'ChessIO | Play and Challenge Others!',
+        description:
+            'ChessIO is a hobby 1v1 chess challenge web app for friends.',
+        locale: 'en_US',
+        type: 'website',
+        images: {
+            url: 'https://chessio.vercel.app/og.png',
+            width: 1200,
+            height: 630,
+        },
+    },
+    metadataBase: new URL('https://chessio.vercel.app'),
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
