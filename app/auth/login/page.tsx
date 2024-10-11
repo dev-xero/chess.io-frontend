6fd0a52b854df1f29f8b736d50c7cb88b9049b76
dev-xero <elo48858@gmail.com>
2024-10-11 23:25:46 +0100

    feat: Implement register page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,15 +5,14 @@ import Header from '@/components/Header';
 import InputField from '@/components/InputField';
 import Link from '@/components/Link';
 import CenteredGrid from '@/layout/CenteredGrid';
-import { Horse, Lock, Asterisk } from '@phosphor-icons/react';
+import { Horse, Lock } from '@phosphor-icons/react';
 import { FormEvent, useState } from 'react';
 
 export default function Page() {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
-    const [secretQuestion, setSecretQuestion] = useState('');
 
-    const handleUserRegistration = (ev: FormEvent<HTMLFormElement>) => {
+    const handleUserLogin = (ev: FormEvent<HTMLFormElement>) => {
         ev.preventDefault();
         alert('Not yet implemented!');
     };
@@ -23,16 +22,14 @@ export default function Page() {
             <section className="md:w-[512px] max-w-lg flex flex-col items-center p-2">
                 <Header />
                 <section className="text-center flex flex-col mt-8">
-                    <h2 className="font-bold mb-2 text-2xl">
-                        Create Your Account
-                    </h2>
+                    <h2 className="font-bold mb-2 text-2xl">Log Back In</h2>
                     <p className="text-faded">
-                        Let&apos;s create an account so you can play on ChessIO!
+                        Log back into your account to play friends!
                     </p>
                 </section>
                 <form
                     action="/"
-                    onSubmit={handleUserRegistration}
+                    onSubmit={handleUserLogin}
                     className="md:w-[512px]"
                 >
                     <section className="my-8 flex flex-col gap-2">
@@ -52,27 +49,15 @@ export default function Page() {
                             onChange={(val) => setPassword(val)}
                             type="password"
                         />
-                        <InputField
-                            name="secret-field"
-                            icon={<Asterisk size={24} />}
-                            placeholder="Secret question"
-                            text={secretQuestion}
-                            onChange={(val) => setSecretQuestion(val)}
-                            type="text"
-                        />
-                        <p className="text-xs text-primary mt-2 text-center">
-                            We ask for a secret question in case you forget your
-                            password.
-                        </p>
                     </section>
                     <FilledButton
-                        label="Register"
-                        onClick={() => handleUserRegistration}
+                        label="Log In"
+                        onClick={() => handleUserLogin}
                     />
                     <section className="mt-4 flex items-center justify-center gap-4">
                         <Link
-                            href="/auth/login"
-                            label="Login Instead"
+                            href="/auth/register"
+                            label="Register Instead"
                             external={false}
                         />
                         <Link
--- components/FilledButton.tsx
+++ components/FilledButton.tsx
@@ -6,7 +6,7 @@ interface IFilledButtonProps {
 export default function FilledButton(props: IFilledButtonProps) {
     return (
         <button
-            className="w-full my-2 p-4 rounded-md bg-primary font-bold hover:opacity-90 transition-all active:scale-[.98]"
+            className="w-full my-2 p-4 rounded-md bg-primary font-bold hover:opacity-90 transition-all active:scale-[.98] text-xl"
             onClick={props.onClick}
         >
             {props.label}
