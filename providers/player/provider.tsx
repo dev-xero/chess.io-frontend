45725aa909ac344cb0249bd0e5a8b5831bd8f0d5
dev-xero <elo48858@gmail.com>
2024-10-14 23:10:08 +0100

    chore: Move use player hook to hooks directory

--- providers/player/provider.tsx
+++ providers/player/provider.tsx
@@ -27,11 +27,3 @@ export const PlayerProvider = ({ children }: IPlayerProviderProps) => {
         </PlayerContext.Provider>
     );
 };
-
-export const usePlayer = () => {
-    const context = useContext(PlayerContext);
-    if (!context) {
-        throw new Error('usePlayer must be used within a PlayerProvider');
-    }
-    return context;
-};
