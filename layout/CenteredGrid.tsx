7dd5e53326b251fdadbc0ab912b3ccc0cb67f7ca
dev-xero <elo48858@gmail.com>
2024-10-30 16:05:05 +0100

    style: Use dvh unit for centered grids

--- layout/CenteredGrid.tsx
+++ layout/CenteredGrid.tsx
@@ -5,5 +5,5 @@ interface ICenteredGridProps {
 }
 
 export default function CenteredGrid({ children }: ICenteredGridProps) {
-    return <main className="w-full h-screen grid place-items-center overflow-x-hidden">{children}</main>;
+    return <main className="w-full h-[100dvh] grid place-items-center overflow-x-hidden">{children}</main>;
 }
